ae3e0a8510918a678f82073e268fe8e0dfd1d9ae
MaximilianHolleran <mhholleran@gmail.com>
2024-02-22 20:40:10 -0500

    activity complete

---
+++ src/components/Gallery.js
@@ -0,0 +1,19 @@
+import GalleryItem from './GalleryItem'
+
+function Gallery (props){
+
+    const display = props.data.map((item, index) => {
+        return (
+            <GalleryItem key={index} item={item} />
+        )
+    })
+
+    return (
+        <div>
+            {display}
+        </div>
+    )
+}
+
+
+export default Gallery
